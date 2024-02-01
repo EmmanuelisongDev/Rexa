d8f11aec19e5c14d44a457186adc944487fa3237
Emmanuel Isong <emmisongdev@gmail.com>
2024-02-01 22:11:01 +0100

    Modified Sign Up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUpSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email address").required("Required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
-    .required("Required"),
+    .required("Password Required"),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required("Required"),
