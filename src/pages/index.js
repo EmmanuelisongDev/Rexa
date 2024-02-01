52ba9a586bf645cac75aba1d95a5ed78e41f5993
Emmanuel Isong <emmisongdev@gmail.com>
2024-02-01 22:21:16 +0100

    Adjusted CheckoutForm.jsx directory in pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Home from "./Home";
 import Product from "./Product";
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
-import CheckoutForm from "./CheckOutForm";
+import CheckoutForm from "./CheckoutForm";
 
 export { Home, Product, SignUp, LogIn, CheckoutForm };
